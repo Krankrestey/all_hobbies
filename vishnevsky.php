ddf38b407645002f5d2cc065830fc907901959a9
Romchela <romchela@mail.ru>
2014-12-23 20:55:15 +0600

    add vishnevsky

---
+++ vishnevsky.php
@@ -0,0 +1,31 @@
+<table align = "center" width = 1000 border = 0>
+	<tr>
+	<td width = 70% valign = top>
+	
+	<!-- information here -->
+	<h3><u>About me</u></h3>
+	I am Vishnevsky Roman. My native country is Russia. I live in Novosibirsk (Siberia). I am studiyng at Novosibirsk State University. 
+	<br><br>
+	<p align = center>
+	<img src = "https://pp.vk.me/c625720/v625720825/c004/DUPyo0SOUlo.jpg" width = 250>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src = "https://pp.vk.me/c625720/v625720825/bffb/vLCRs-ebkvk.jpg" width = 300></p>
+	My hobbies is olympiad programming. The main goal of competitions is solving problems faster as possible.
+	It is popular in our time. There are many web-sites where you can training your skills in this field of science.
+	I often participate in international competitions.
+	<br><br>
+	For example, <a href = "codeforces.com/profile/romchela">Codeforces</a>. You have 5 problems and 2 hours for solving them. After contest you will have a rating. You can see my progress at
+	the picture below.<br><br>
+	<center><img width = 100% src = "http://i64.fastpic.ru/big/2014/1204/b2/1df65e19359b53d10ec050ecc6abffb2.png"></center>
+	<br><br>
+	There are many achieve of problems where you can train everytime and everywhere. For example, <a href = "acmp.ru">ACMP</a>, <a href = "acm.timus.ru">Timus</a> and much more.
+	
+	<br><br>
+	<center><img src = http://campuscurico.utalca.cl/~foro-icpc/wiki/styles/icpc/logo.png></center><br>
+	We have ACM ICPC. It is World Cup of the olympiad programming. You have three persons, one computer and 5 hours for solving 12 problems. We try hard to qualify for the final. It is the main my goal now. It is prestigious and in the future these achievements can help you with work.
+	<br><br><center><img src = "https://pp.vk.me/c625720/v625720825/bff2/ybcPuhsY1yg.jpg" height = 400></center>
+	<br>This is my team from my university. This picture was made in time of fall training camp in MIPT, Moscow.<br>
+	</td>
+	<td width = 30% valign = top>
+	<img src = "files/iam.png" width = 100%>
+	</td>
+	</tr>
+</table>
