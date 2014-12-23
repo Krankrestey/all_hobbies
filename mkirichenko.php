db89ec9997e684343e94c2ddd39c3962d1daffa5
Krankrestey <opengithub@yandex.ru>
2014-12-23 21:59:28 +0600

    Add hobby

---
+++ mkirichenko.php
@@ -0,0 +1,37 @@
+<html>
+    <?php include 'header.html';?>
+    <head>
+        <meta charset="UTF-8">
+        <title>Хобби</title>
+    </head>
+    <body background="/~saltuk/allhobbies/images/fon.jpg">
+		<div style="width:70%; margin:0 auto; background:rgba(255,255,255,0.7); border:solid 1px #aaa; border-radius:20px; margin-top:50px; margin-bottom:50px; padding:20px; font-family:Helvetica">
+		<p style="font-size:40;" align="center">Мое хобби</p>
+		<p style="word-break: break-all; margin-bottom: 1; margin-top: 0;">Мне тяжело выделить что-то одно как хобби. Так что к хобби я решил отнести то, на что я потратил больше всего своего времени.</p>
+		<br>
+		<p style="word-break: break-all; margin-top: 3; margin-bottom: 3;"><img src="./images/mkirichenko_1.jpg" width="70%"></p>
+		<p style="margin-bottom: 1; font-size:15;"><i>Время игры.</i></p>
+		<br>
+		<p style="word-break: break-all; margin-bottom: 1; margin-top: 0;">Начал играть в ролевые игры я еще до этого, но именно "скайрим" заставил меня сидеть у компьютера часами. Что же привлекло меня? Поначалу было интересно замечать какие-то различия с предыдущими играми, затем я стал бродить по миру, изучать его. Здесь он был очень правдоподобен, хотя и мал. К сожалению жители мира были не так интересны: однообразные реплики, мало возможностей для взаимодействия.</p>
+		<br>
+		<p style="word-break: break-all; margin-top: 3; margin-bottom: 3;"><img src="./images/mkirichenko_2.jpg" width="70%"></p>
+		<p style="margin-bottom: 1; font-size:15;"><i>Убитая лиса.</i></p>
+		<br>
+		<p style="word-break: break-all; margin-bottom: 1; margin-top: 0;">Сюжетная часть увлекла меня не сразу. Я обошел всю карту, выполнял какие-то посторонние задания, которы были достаточно трудны. К какому-то времени стало понятно, что дальше игнорировать сюжет нельзя, потому что самое интересное было недоступно. Начав выполнять главные задания стало понятно, что настало время самого главного в любой ролевой игре: прокачки навыков. На это я потратил достаточно много времени.</p>
+		<br>
+		<p style="word-break: break-all; margin-top: 3; margin-bottom: 3;"><img src="./images/mkirichenko_3.jpg" width="70%"></p>
+		<p style="margin-bottom: 1; font-size:15;"><i>Результат прокачки.</i></p>
+		<br>
+		<p style="word-break: break-all; margin-bottom: 1; margin-top: 0;">После этого прохождение стало легким. Я уже не тратил на игру так много времени. Остались только побочные задания, дополнительные сюжетные линии, которы оказались даже несколько интереснее основной. </p>
+		<br>
+		<p style="word-break: break-all; margin-top: 3; margin-bottom: 3;"><img src="./images/mkirichenko_4.jpg" width="70%"></p>
+		<p style="margin-bottom: 1; font-size:15;"><i>Вечер в игре.</i></p>
+		<br>
+		<p style="word-break: break-all; margin-bottom: 1; margin-top: 0;">К игре выходили дополнения, которые немедленно покупались и проходились. Когда уже совсем не осталось заданий, я начал выполнять достижения в игре, просто, также, как и в начале игры, ходить по миру, охотиться на мелких животных, обворововать жителей, разграблять города. Сейчас я уже не играю, но обязательно вернусь к игре когда-нибудь.й</p>
+		<br>
+		<p style="word-break: break-all; margin-top: 3; margin-bottom: 3;"><img src="./images/mkirichenko_5.jpg" width="70%"></p>
+		<p style="margin-bottom: 1; font-size:15;"><i>Лучшее в игре.</i></p>
+		<br>
+</div>
+</body>
+</html>
