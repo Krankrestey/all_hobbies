bf448ab5bfb9589e4207085c27584b3f3797a8b2
Krankrestey <opengithub@yandex.ru>
2014-12-23 19:59:13 +0600

    Some corrections

---
+++ chernova/animals.php
@@ -0,0 +1,31 @@
+﻿<html>
+	<head>
+                <title>Животные из бисера</title>
+	</head>
+	<body>
+		
+		<?php 
+			include '../header.html';
+		?>
+		<body background="../images/fon.jpg">
+		<b><font size="+3"><font color="990066"><center>В этом разделе вы можете найти схемы плетения различных животных.</center></font></font></b>
+		<br>
+		<p align="center"><table width=1250>
+			<tr>
+				<td>
+		<u><b><font size="+2"><font color="990066">Коала</font></font></b></u><br>
+		<img src="/~chernova/public_html/hobby/scheme_coala.JPG" width="800"><img src="/~chernova/public_html/hobby/coala.JPG" width="400"><br><br>
+		<u><b><font size="+2"><font color="990066">Обезьянка</font></font></b></u><br>
+		<img src="/~chernova/public_html/hobby/scheme_monkey.JPG" width="800"><img src="/~chernova/public_html/hobby/monkey.JPG" width="400"><br><br>
+		<u><b><font size="+2"><font color="990066">Петушок</font></font></b></u><br>
+		<img src="/~chernova/public_html/hobby/scheme_chick.JPG" width="800"><img src="/~chernova/public_html/hobby/chick.JPG" width="400"><br><br>
+		<u><b><font size="+2"><font color="990066">Слоник</font></font></b></u><br>
+		<img src="/~chernova/public_html/hobby/scheme_elephant.JPG" width="800"><img src="/~chernova/public_html/hobby/elephant.JPG" width="400"><br><br>
+		<u><b><font size="+2"><font color="990066">Жираф</font></font></b></u><br>
+		<img src="/~chernova/public_html/hobby/scheme_giraffe.JPG" width="800"><img src="/~chernova/public_html/hobby/giraffe.JPG" width="400"><br><br>
+		<p align="right"><b><font size="+2"><a href="../chernova.php">Назад</a></font></b></p>
+				</td>
+			<tr>
+		</table></p>
+	</body>
+</html>
