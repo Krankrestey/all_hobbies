134a3b12ccb339a856c13f544fde0fbf71597db9
vasyaelvis <im@ccfit.nsu.ru>
2014-12-24 23:29:48 +0700

    1

---
+++ im.php
@@ -0,0 +1,149 @@
+<?php include 'header.html'; ?>
+<style>
+body 
+{
+background-color:#000000;
+background-image: url(vorota.jpg);
+ background-repeat: no-repeat;
+    background-position: center top; overflow: auto;
+background-attachment: scroll;
+}
+text-align: center;
+scrolling="yes";
+
+table tbody{
+border-bottom: 3px solid #000;
+}
+
+</style>
+</head>
+
+<body align = "center" >
+	
+<p align = "center"> 
+<font color="#CDC9C9" size="10" face="arial"> 
+ Hobby Page
+<br >
+</font>
+<font color="#CDC9C9" size="8" face="arial">
+Im Maxim
+</font>
+</p>
+
+
+
+<table border="2" bgcolor="#FC1D24" cellspacing="2" width="300px" height="70px" align="center">
+
+<tr>
+<td><p align="center"><font color="#424242" size="7">LIVERPOOL</font></p></td>
+</tr></table>
+
+<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<br>
+<style>
+div.radius{
+	border-radius: 20px;
+	background: #C74448;
+	margin: auto;
+	width:750px;
+}
+
+div.rad{
+	border-radius: 20px;
+	background: #EEE5DE;
+	margin: auto;
+	width:750px;
+}
+
+</style>
+
+<div class="radius"><p align="center"><font color="#424242" size="5" face="arial">If you ask me about my hobby, I'll immediatly answer: FOOTBALL!!<br>
+I play football since I was 9 and really love it. Of course I have favourite Football Club which I support.<br>
+This club is Liverpool FC. It a lot what I can say about it, but it's easier to see with your own eyes here!!!<br>
+<a href = "http://www.liverpoolfc.ru/">Liverpool FC Official site in Russia<br></a>
+I support this club and think that's my main hobby.
+</font>
+</p></div>
+<br><br><br>
+
+<div class="rad"><p align="center"><font color="#424242" size="5" face="arial">
+Big Trophies<br><br>
+League Champions:<br>
+1900-01, 1905-06, 1921-22, 1922-23, 1946-47, 1963-64, 1965-66, 1972-73, 1975-76,<br>
+1976-77, 1978-79, 1979-80, 1981-82, 1982-83, 1983-84, 1985-86, 1987-88, 1989-90<br>
+<br>
+
+European Cup Winners:<br>
+1976-77, 1977-78, 1980-81, 1983-84, 2004-05<br>
+<br>
+
+FA Cup Winners:<br>
+1964-65, 1973-74, 1985-86, 1988-89, 1991-92, 2000-01, 2005-06<br>
+<br>
+
+League Cup Winners:<br>
+1980-81, 1981-82, 1982-83, 1983-84, 1994-95, 2000-01, 2002-03, 2011-2012<br>
+<br>
+
+UEFA Cup Winners:<br>
+1972-73, 1975-76, 2000-01
+</font></p></div>
+<br><br><br><br><br>
+
+
+<div class="radius"><p align="center"><font color="#424242" size="5" face="arial">
+My favourite player - Steven Gerrard,  a legend of Liverpool, England National Team and football in general.
+</font></p></div>
+
+<table border="0" cellspacing="0" width="500px" height="800px" align="center">
+<tr>
+<td align="center" valign = top>
+	<img src = "image/im_istanbul.jpg" width = 700px align="center">
+	</td></tr></table>
+
+<br><br>
+
+<div class="radius"><p align="center"><font color="#424242" size="5" face="arial">
+I started support Liverpool because of their character.<br> They never give up. <br>
+Match in Istanbul is the best match in Liverpool's and I think in football's history.<br>
+Milan were winnig 3-0 in first half but Liverpool scored 3 in second half and won in penalties!!<br>
+Steve Gerrard was the MVP(most valueble player). He scored first goal and earned the penalty which scored Xabi Alonso.
+</font></p></div>
+
+</table>
+
+
+<br>
+<br>
+<br>
+<br>
+<p align = "center">
+<iframe width="800" height="470"
+src="//www.youtube.com/embed/4ZFHwJMIWUA" frameborder="0" allowfullscreen>
+</iframe>
+</p>
+
+
+<br><br><table border="0" cellspacing="0" width="800px" height="500px" align="center">
+<tr>
+
+<td align="center" valign = top>
+	<img src = "image/im_car.jpg" width = 800px>
+	</td></tr></table>
+
+
+<br>
+<br>
+<br><br><br>
+
+<div class="rad"><p align="center"><font color="#424242" size="5" face="arial">
+LFC is one of the most titled club of England with huge history. It is great, grand club and <br>
+if you think what club to support - think about Liverpool, it's good choice!
+</font></p></div>
+
+
+<table border="0" cellspacing="0" width="800px" height="500px" align="center">
+<tr>
+<td align="bottom" valign = top>
+	<img src = "image/im_adidas.jpg" width = 800px>
+	</td></tr></table>
